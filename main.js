a04d0bc5aa6f7cb2b9843ab423eb71df1c4a39bf
Melanie Arellano <melaniesigrid@protonmail.com>
2021-12-14 01:11:59 -0600

    Correct style linter errors

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-    const body = document.querySelector("body");
-    const navbar = document.querySelector(".navbar");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-    }
-    cancelBtn.onclick = ()=>{
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-    }
+const body = document.querySelector('body');
+const navbar = document.querySelector('.navbar');
+const menuBtn = document.querySelector('.menu-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+
+menuBtn.onclick = () => {
+  navbar.classList.add('show');
+  menuBtn.classList.add('hide');
+};
+
+cancelBtn.onclick = () => {
+  navbar.classList.remove('show');
+  menuBtn.classList.remove('hide');
+};
+
 window.addEventListener('scroll', function () {
   let navbar = document.querySelector('nav');
   let logoImage = document.getElementById('logo-image');
   let windowPosition = window.scrollY > 40;
   navbar.classList.toggle('scroll-active', windowPosition);
   logoImage.classList.toggle('logo-scroll', windowPosition);
-})
+});
